3670bd0e89a939e1e2f951fa92ca14d622fa9e69
matt <matt@074946ed-f030-0410-aafb-900324183ce0>
2007-06-09 15:54:23 +0000

    - a little refactoring - new 'pagelist' tab command

--- textmate-wiki/trunk/Plain Text Wiki.tmbundle/Support/lib/wiki.rb
+++ textmate-wiki/trunk/Plain Text Wiki.tmbundle/Support/lib/wiki.rb
@@ -4,12 +4,14 @@
 # $LastChangedDate$
 
 require 'FileUtils'
+require 'uri'
 
 $: << "#{ENV['TM_SUPPORT_PATH']}/lib" if ENV.has_key?('TM_SUPPORT_PATH')
 
 class PlainTextWiki
-    # Default extension
+    # Default extension for page creation
     EXT = ".txt"
+    EXPORT_FORMAT = "markdown" # "markdown" and "textile" are recognised
     
     # set by the initializer, passed in
     attr_reader :dir
@@ -32,9 +34,7 @@ class PlainTextWiki
        if ENV['TM_SCOPE'].include?('markup.other.pagename.delimited')
         	idx = ENV['TM_LINE_INDEX'].to_i
         	pagename = (((ENV['TM_CURRENT_LINE'][0..idx-1] || "").reverse)[/^[^\[]*/] || "").reverse + ENV['TM_CURRENT_LINE'][idx..-1][/^[^\]]*/]
-        	pagename = pagename.sub(/\[+/, "");
-        	pagename = pagename.sub(/\]+/, "");
-        	pagename.capitalize!
+        	pagename.tr("[]", "").capitalize!
         else
         	pagename = ENV['TM_CURRENT_WORD']
         end
@@ -47,79 +47,70 @@ class PlainTextWiki
     end
 
     def go_to(pagename)
-        fn = pagename + EXT
-        
-        require 'uri'
-
         # Touch the file if it doesn't exist
-        all_files = Dir.entries(dir)
-        unless all_files.include? fn
+        unless pages.include? pagename
             # It may be the file exists but with a different case
-            if all_files.map { |af| af.downcase }.include? fn.downcase
+            if pages.map { |p| p.downcase }.include? pagename.downcase
                 # The filename is needed with the correct case because
                 # otherwise it won't open properly in the project window
-                fn = all_files.select { |af| af.downcase == fn.downcase }.first
+                pagename = pages.select { |p| p.downcase == pagename.downcase }.first
             else
-                FileUtils.touch("#{dir}/#{fn}")
+                FileUtils.touch("#{dir}/#{pagename}#{EXT}")
                 # switch away from TextMate and back to refresh the project drawer
         	    `osascript -e 'tell application "Dock" to activate'; osascript -e 'tell application "TextMate" to activate'`
             end
         end
 
-        fn = "#{dir}/#{fn}"
+        fn = "#{dir}/#{pagename}#{EXT}"
         `open "txmt://open/?url=file://#{URI.escape(fn, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"`    
     end
     
+    def linked_page_list
+        pages.map { |p| p.include?(" ") ? "* [[#{p}]]" : "* #{p}" }.join("\n")
+    end
+    
     def export_as_html
-        require 'Find'
-        require "#{ENV['TM_SUPPORT_PATH']}/lib/bluecloth.rb"
-        require "#{ENV['TM_SUPPORT_PATH']}/lib/rubypants.rb"
-
-        # Ask the user for an export directory, exiting if cancelled
-        cocoadialog = "#{ENV['TM_SUPPORT_PATH']}/bin/CocoaDialog.app/Contents/MacOS/CocoaDialog"
-        export_dir = `#{cocoadialog} fileselect --text "Choose a directory for wiki export" --select-only-directories`.strip
-        exit if export_dir.empty?
-
-        # Gather a list of source files, from the top level only
-        files = [] # ([source, dest, title])
-        extensions = ['.txt', '.markdown', '.mdown', '.markdn', '.md']
-        Find.find(dir) do |path|
-        	next if path == dir
-        	File.prune if File.directory?(path)
-        	next unless extensions.include?(File.extname(path))
-
-        	old_fn = File.split(path)[1]
-        	title = old_fn[0..(old_fn.length-File.extname(old_fn).length-1)]
-        	new_fn = title + '.html'
-        	files.push([path, File.join(export_dir, new_fn), title])
-        end
-
-        # Make sure there are no files in the way
-        files_in_the_way = false
-        files.each do |source, dest, title|
-        	if File.file?(dest)
-        	    files_in_the_way = true
-       	    end
-        end
-        if File.file?("#{export_dir}/wiki-styles.css")
-            files_in_the_way = true
+        case EXPORT_FORMAT
+          when "markdown"
+            require "#{ENV['TM_SUPPORT_PATH']}/lib/bluecloth.rb"
+            require "#{ENV['TM_SUPPORT_PATH']}/lib/rubypants.rb"
+            transform = Proc.new { |s| RubyPants.new(BlueCloth.new(s).to_html).to_html }
+          when "textile"
+            require "#{ENV['TM_SUPPORT_PATH']}/lib/redcloth.rb"
+            transform = Proc.new { |s| RedCloth.new(s).to_html }
         end
         
-        if files_in_the_way
-            res = `#{cocoadialog} msgbox --text "Export will replace files" --icon "x" --informative-text "There are files in the way in the export directory. They will be lost if you continue." --button1 "Cancel Export" --button2 "Replace All"`
-            if res == 1
+        export_ext = ".html"
+
+        # dialogs
+        cocoadialog = "#{ENV['TM_SUPPORT_PATH']}/bin/CocoaDialog.app/Contents/MacOS/CocoaDialog"
+        export_dir_dialog = %Q[#{cocoadialog} fileselect --text "Choose a directory for wiki export" --select-only-directories]
+        replace_dialog = %Q[#{cocoadialog} msgbox --text "Export will replace files" --icon "x" --informative-text "There are files in the way in the export directory. They will be lost if you continue." --button1 "Cancel Export" --button2 "Replace All"]
+
+        # Ask the user for an export directory, exiting if cancelled
+        export_dir = `#{export_dir_dialog}`.strip
+        exit if export_dir.empty?
+
+        # Make sure there are no files in the way
+        obstructing = (pages + ["#{export_dir}/wiki-styles.css"]).select { |p|
+            File.file?("#{export_dir}/#{p}#{export_ext}")
+        }
+                
+        unless obstructing.empty?
+            res = `#{replace_dialog}`.strip
+            unless res == '2'
+                puts res
                 puts "Cancelled Export Wiki as HTML"
                 exit 206
             end
         end
 
         # For each file, HTML-ify the links, convert to HTML using Markdown, and save
-        files.each do |source, dest, title|
-        	s = with_html_links(open(source, 'r').read)
-        	html = RubyPants.new(BlueCloth.new(s).to_html).to_html
+        pages.each do |p|
+        	html = transform.call(with_html_links(open("#{dir}/#{p}#{EXT}", 'r').read))
 
-        	File.open(dest, 'w') { |fh|
-        		fh.puts(wiki_header % title)
+        	File.open("#{export_dir}/#{p}#{export_ext}", 'w') { |fh|
+        		fh.puts(wiki_header % p)
         		fh.puts(html)
         		fh.puts(wiki_footer)
         	}
@@ -129,8 +120,7 @@ class PlainTextWiki
         FileUtils.copy("#{wiki_styles_path}", "#{export_dir}/wiki-styles.css")
 
         # Open the exported wiki in the default HTML viewer
-        front = File.join(export_dir, "IndexPage.html")
-        `open #{front}`
+        `open #{export_dir}/IndexPage#{export_ext}`
     end
    
     # protected instance methods
@@ -143,10 +133,9 @@ class PlainTextWiki
     end
     
     def load_pages
-        extensions = [EXT]
         all_files = Dir.entries(dir)
         all_files.reject! { |fn| File.directory?("#{dir}/#{fn}") }
-        all_files.reject! { |fn| ! extensions.include?(File.extname(fn)) }
+        all_files.reject! { |fn| File.extname(fn) != EXT }
         all_files.map! { |fn| fn[0..(fn.length-File.extname(fn).length-1)] }
         all_files.sort
     end
@@ -173,18 +162,22 @@ class PlainTextWiki
     def with_html_links(s)
         # This match recognises HTML links, and delimited then camelcase
         # pagenames. Each is treated differently
-        # $1: HTML capture
-        # $2: Delimited capture ($3 is the page name)
-        # $4: Camelcase capture
-        s.gsub(/(<a .+<\/a>)|(\[\[(.+)\]\])|(\b([A-Z][a-z]+([A-Z][a-z]*)+)\b)/) { |m|
+        s.gsub(
+            / (<a .+?<\/a>) # 1, HTML capture
+            | ((http:\/\/.+?)(\s|$)) # 2, 3, 4 http construct
+            | (\[\[(.+?)\]\]) # 5, 6, delimited capture
+            | (\b([A-Z][a-z]+([A-Z][a-z]*)+)\b) # 7 camelcase
+            /x ) { |m|
             if $1
                 $1
+            elsif $3
+                %Q[<a href="#{URI.escape($3)}">#{URI.escape($3)}</a>#{$4}]
             else
-                pagename = $2 ? $2.tr("[]", "").capitalize : $4
+                pagename = $6 ? $6.tr("[]", "").capitalize : $7
                 if (!pages.include?(pagename)) and (pages.map { |p| p.downcase }.include? pagename.downcase)
                     pagename = pages.select { |p| p.downcase == pagename.downcase }.first
                 end 
-                "<a href=\"#{pagename}.html\">#{pagename}</a>"
+                %Q[<a href="#{pagename}.html">#{pagename}</a>]
             end
         }
     end
